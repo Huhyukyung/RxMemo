888933cd28ce3ab10efd3e3df040f5523819b43d
Yukyung Huh <huhyukyung@gmail.com>
2023-08-03 19:29:42 +0900

    add MemoryStorage

---
+++ RxMemo/Service/MemoStorageType.swift
@@ -0,0 +1,23 @@
+//
+//  MemoStorageType.swift
+//  RxMemo
+//
+//  Created by Yukyung Huh on 2023/08/03.
+//
+
+import Foundation
+import RxSwift
+
+protocol MemoStorageType {
+    @discardableResult
+    func createMemo(content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func memoList() -> Observable<[Memo]>
+    
+    @discardableResult
+    func update(memo: Memo, content: String) -> Observable<Memo>
+    
+    @discardableResult
+    func delete(memo: Memo) -> Observable<Memo>
+}
---
+++ RxMemo/Service/MemoryStorage/MemoryStorage.swift
@@ -0,0 +1,60 @@
+//
+//  MemoryStorage.swift
+//  RxMemo
+//
+//  Created by Yukyung Huh on 2023/08/03.
+//
+
+import Foundation
+import RxSwift
+
+class MemoryStorage: MemoStorageType {
+    
+    private var list = [
+        Memo(content: "Hello, RxSwift", insertDate: Date().addingTimeInterval(-10)),
+        Memo(content: "Lorem Ipsum", insertDate: Date().addingTimeInterval(-20))
+    ]
+    
+    private lazy var store = BehaviorSubject<[Memo]>(value: list)
+    
+    @discardableResult
+    func createMemo(content: String) -> RxSwift.Observable<Memo> {
+        let memo = Memo(content: content)
+        list.insert(memo, at: 0)
+        
+        store.onNext(list)
+        
+        return Observable.just(memo)
+    }
+    
+    @discardableResult
+    func memoList() -> RxSwift.Observable<[Memo]> {
+        return store
+    }
+    
+    @discardableResult
+    func update(memo: Memo, content: String) -> RxSwift.Observable<Memo> {
+        let updated = Memo(original: memo, updatedContent: content)
+        
+        if let index = list.firstIndex(where: { $0 == memo }) {
+            list.remove(at: index)
+            list.insert(updated, at: index)
+        }
+        
+        store.onNext(list)
+        
+        return Observable.just(updated)
+    }
+    
+    @discardableResult
+    func delete(memo: Memo) -> RxSwift.Observable<Memo> {
+        if let index = list.firstIndex(where: { $0 == memo }) {
+            list.remove(at: index)
+        }
+        
+        store.onNext(list)
+        
+        return Observable.just(memo)
+    }
+    
+}
